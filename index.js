1e4ef6e0ab8b3c096e5145687dec388ac42e0465
Khanyie1 <khanyisanimajozi98@gmail.com>
2024-07-17 13:25:12 +0100

    decimal value change is fixed

--- index.js
+++ index.js
@@ -124,8 +124,8 @@ document.addEventListener("alpine:init", () => {
                             this.message = result.data.message + " Sorry - that is not enough money!";
                             setTimeout(() => this.message = '', 6000)
                         } else if (result.data.status == "success") {
-                            const change = this.paymentAmount - this.cartTotal.toFixed(2);
-                            this.message = `Payment received, but you have change of : R${change} Enjoy your Pizzas!`
+                            const change = this.paymentAmount - this.cartTotal;
+                            this.message = `Payment received, but you have change of : R${change.toFixed(2)} Enjoy your Pizzas!`
 
                             setTimeout(() => {
                                 this.message = '';
